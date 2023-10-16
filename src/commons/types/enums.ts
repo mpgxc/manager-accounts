ba14079733195e5ffa70f369eb9d3f32c5d4c58e
mpgxc <mpgx5.c@gmail.com>
2023-10-16 10:34:31 -0300

    fix(enums): ajustes na nomeclatura

--- src/commons/types/enums.ts
+++ src/commons/types/enums.ts
@@ -1,14 +1,15 @@
-const RegexPatternsEnum = Object.freeze({
+const RegexPatterns = Object.freeze({
   AUTHORIZATION_HEADER:
     /^Bearer [A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.[A-Za-z0-9-_.+/=]*$/,
 
   TENANT_HEADER: /^tenant_[a-zA-Z0-9-]+$/,
 });
 
-export enum CacheTTL {
+enum CacheTTL {
   PERSIST = 0,
   ONE_MINUTE = 60 * 1000,
   ONE_HOUR = 60 * 60 * 1000,
   ONE_DAY = 24 * 60 * 60 * 1000,
 }
 
+export { CacheTTL, RegexPatterns };
--- src/infra/http/auth/guards/permissions.guard.ts
+++ src/infra/http/auth/guards/permissions.guard.ts
@@ -3,16 +3,17 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import {
   PERMISSIONS_KEY,
-  PermissionsUnion,
+  Permissions,
 } from '../decorators/permissions.decorator';
 @Injectable()
 export class PermissionsGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredPermissions = this.reflector.getAllAndOverride<
-      PermissionsUnion[]
-    >(PERMISSIONS_KEY, [context.getHandler(), context.getClass()]);
+    const requiredPermissions = this.reflector.getAllAndOverride<Permissions[]>(
+      PERMISSIONS_KEY,
+      [context.getHandler(), context.getClass()],
+    );
 
     if (!requiredPermissions) {
       return true;
